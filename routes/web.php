3b3efe1dcd20fab85f568771905feeea4ebd7ba9
marie.haeusgen <marie.haeusgen@iu.org>
2025-03-19 15:11:18 +0100

    wip manny

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('tree-admin');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
